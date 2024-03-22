50eb0058177ab4504be20563d3ca1fe3e5087017
jonathanpaul2004 <142970303+jonathanpaul2004@users.noreply.github.com>
2024-03-22 16:22:02 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,15 +1,15 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Assuming your MySQL credentials
-    $servername = "localhost";
-    $username = "root"; // Your MySQL username
-    $password = ""; // Your MySQL password
-    $dbname = "login_register"; // Your MySQL database name
 
-    // Create connection
+    $servername = "localhost";
+    $username = "root"; 
+    $password = ""; 
+    $dbname = "login_register"; 
+
+    
     $conn = new mysqli($servername, $username, "", $dbname);
 
-    // Check connection
+    
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
@@ -17,19 +17,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    // Perform SQL query to check login credentials
+ 
     $sql = "SELECT * FROM userlog WHERE email='$email' AND password='$password'";
     $result = $conn->query($sql);
 
     if ($result->num_rows >= 1) {
         echo "Login successful";
-        //header("Location: profile.html");
-        // You can redirect the user to another page here if login is successful
+       
     } else {
         echo "Login failed. Invalid email or password.";
     }
 
-    // Close connection
+    
     $conn->close();
 }
 ?>
